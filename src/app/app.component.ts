043528561819a9bbd6e1851c5087bd0afbb34ed0
Thiago F <thiago.falcao86@gmail.com>
2023-02-14 01:28:30 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  num1!: number;
-  num2!: number;
-  num3!: number;
-  num4!: number;
-  num5!: number;
-  num6!: number;
+  title = 'random-numbers';
+  num1 = 0;
+  num2 = 0;
+  num3 = 0;
+  num4 = 0;
+  num5 = 0;
+  num6 = 0;
 
-  gerarNumeros() {
-    this.num1 = Math.floor(Math.random() * 60) + 1;
-    this.num2 = Math.floor(Math.random() * 60) + 1;
-    this.num3 = Math.floor(Math.random() * 60) + 1;
-    this.num4 = Math.floor(Math.random() * 60) + 1;
-    this.num5 = Math.floor(Math.random() * 60) + 1;
-    this.num6 = Math.floor(Math.random() * 60) + 1;
+  generateRandomNumber(): number {
+    return Math.floor(Math.random() * 60) + 1;
   }
-  
+
+  generateNumbers(): void {
+    let numbers: number[] = [];
+    let currentNumber;
+
+    do {
+      currentNumber = this.generateRandomNumber();
+      if (!numbers.includes(currentNumber)) {
+        numbers.push(currentNumber);
+      }
+    } while (numbers.length < 6);
+
+    this.num1 = numbers[0];
+    this.num2 = numbers[1];
+    this.num3 = numbers[2];
+    this.num4 = numbers[3];
+    this.num5 = numbers[4];
+    this.num6 = numbers[5];
+  }
 }
