f2754ea4ea81b070edd1824a35156b84323ba83f
Thiago F <thiago.falcao86@gmail.com>
2023-02-14 00:54:36 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sortedNumber';
+  numeros: number[] = [];
+
+  gerarNumeros() {
+    this.numeros = [];
+    for (let i = 0; i < 6; i++) {
+      const numero = Math.floor(Math.random() * 60) + 1;
+      this.numeros.push(numero);
+    }
+  }
 }
